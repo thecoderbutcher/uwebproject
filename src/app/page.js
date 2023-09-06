3e5637070ac8a8a3bfe2d320da433adb9640c539
Ignacio Morinigo <morinacho@gmail.com>
2023-09-06 13:32:01 -0300

    feat:main structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,26 @@
+// Components
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Carees from "./components/Careers";
+import About from "./components/About";
+import Why from "./components/Why";
+import Testimonial from "./components/Testimonial";
+import Cta from "./components/Cta";
+import Footer from "./components/Footer";
+import BackToTopBtn from "./components/BackToTopBtn";
+
 export default function Home() {
   return (
-    <main className="">
-      main
+    <main className="max-w-[1920px] bg-white mx-auto relative overflow-hidden">
+      <Header />
+      <Hero />
+      <Carees />
+      <About />
+      <Why />
+      <Testimonial />
+      <Cta />
+      <Footer />
+      <BackToTopBtn />
     </main>
   )
 }
