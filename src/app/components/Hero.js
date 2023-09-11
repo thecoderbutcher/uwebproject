3615971451a54827a94c2c60fb36875db22039e5
Ignacio Morinigo <morinacho@gmail.com>
2023-09-11 00:57:37 -0300

    feat: hero page and icons

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -3,6 +3,7 @@
 import { useContext } from "react";
 //components
 import Search from "./Search";
+import Image from "next/image";
 //context
 import { SearchContext } from "../context/search.js";
 
@@ -12,7 +13,25 @@ export default function Hero() {
   return (
     <section className="h-screen xl:h-[90vh] bg-[#b2b7c2]/10" id="home">
       <div className="container mx-auto h-full xl:pt-10">
-        
+        <div className="flex flex-col xl:flex-row justify-center items-center xl:justify-start h-full">
+          <div className="text-center xl:max-w-xl xl:text-left mt-16 xl:mt-0">
+            <h1 className="h1">
+              <span className="text-accent-default">UPRO</span> Universidad Provincial de Oficios{''}
+            </h1>
+            <p className="description max-w-[550px] mx-auto xl:mx-0 mb-6 xl:mb-10">La Universidad de oficios más importante de latinoamérica</p>
+            <div className="flex gap-x-3 justify-center xl:justify-start">
+              <button className="btn-cta">
+                <Image src={'/icons/playstore-btn.svg'} width={132} height={36} alt=""/>
+              </button>
+              <button className="btn-cta">
+                <Image src={'/icons/appstore-btn.svg'} width={132} height={36} alt=""/>
+              </button>
+            </div>
+          </div>
+          <div className="relative w-full h-full max-h-[50vh] md:max-w-[70vw] xl:max-w-[860px] xl:max-h-[542px] xl:absolute xl:-right-[100px] min-[1680px] :right-[120px] xl:top-48">
+            <Image src={'/images/tanque.png'} fill style={{objectFit:'contain'}} alt="" priority/>
+          </div>
+        </div>
       </div>
       {
         searchActive ? 
--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -6,6 +6,7 @@ import { SearchContext } from "../context/search";
 //components 
 import LocationSelection from "./LocationSelection";
 import DateSelection from "./DateSelection";
+import HoursSelection from "./HoursSelection";
 
 export default function Search() {
   const {searchActive } = useContext(SearchContext);
@@ -13,9 +14,14 @@ export default function Search() {
   return (
     <div className={`${searchActive ? 'bg-white rounded-none xl:h-[80px]':'bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]'} 
       hidden xl:block w-full relative shadow-lg `}>
-      <div className="xl:h-full flex items-center px-6 xl:px-0">
+      <div className={`flex h-full ${searchActive && 'container mx-auto'} `}>
         <LocationSelection/>
         <DateSelection />
+        <HoursSelection />
+        {/* Search btn */}
+        <div className="xl:h-full flex items-center px-6 xl:px-0">
+          <button className={`${searchActive ? 'btn btn-sm btn-accent xl:w-[164px]': 'btn btn-lg btn-accent xl:w-[184px]'}`}>Buscar</button>
+        </div>
       </div>
     </div>
   )
